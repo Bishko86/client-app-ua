52ab8a7b1388da59d467423aef3d1f0c8ae0b74c
Bishko86 <bishko86@yahoo.com>
2022-04-04 21:30:21 +0300

    #24 | handle the cases of expired refreshToken

--- src/app/interceptors/api.interceptor.ts
+++ src/app/interceptors/api.interceptor.ts
@@ -12,14 +12,19 @@ import { ErrorStatus } from '../common/enums/error-status.enum';
 import { catchError, switchMap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 import { ITokenPair } from '../common/interfaces/auth.interface';
+import { Store } from '@ngrx/store';
+import { IAppState } from '../store/states/app.state';
+import { Logout } from '../modules/auth/store/auth.actions';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
   apiUrl = environment.production
     ? 'http://137.184.2.106:8080'
     : 'http://127.0.0.1:8080';
+  private isRefreshing = false;
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private store: Store<IAppState>
   ) {}
   intercept(
     request: HttpRequest<unknown>,
@@ -58,36 +63,43 @@ export class ApiInterceptor implements HttpInterceptor {
     accessToken: string
   ): HttpRequest<any> {
     return req.clone({
-      withCredentials: true,
       headers: req.headers.set('Authorization', `Bearer ${accessToken}`),
     });
   }
-  private checkErrorStatus(
-    errStatusCode: number,
-    statusCodesToVerify: number[]
-  ): boolean {
+
+  private checkErrorStatus(errStatusCode: number, statusCodesToVerify: number[]): boolean {
     return statusCodesToVerify.some((code) => errStatusCode === code);
   }
 
-  private handleUnauthorized(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+  private handleUnauthorized(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const refreshToken = localStorage.getItem('refreshToken') || '';
-    return this.getNewTokens(refreshToken).pipe(
-      catchError((err) => {
-        return of(err);
-      }),
-      switchMap((tokens) => {
-        localStorage.setItem('accessToken', tokens.accessToken);
-        return next.handle(
-          this.addAccessTokenToHeader(req, tokens.accessToken)
-        );
-      })
-    );
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+      return this.getNewTokens(refreshToken).pipe(
+        switchMap((tokens) => {
+          this.isRefreshing = false;
+          localStorage.setItem('accessToken', tokens.accessToken);
+          localStorage.setItem('refreshToken', tokens.refreshToken);
+          return next.handle(
+            this.addAccessTokenToHeader(req, tokens.accessToken)
+          );
+        }),
+        catchError((err: HttpErrorResponse) => this.handleRefreshTokenIsNotValid())
+      );
+    } else {
+      return of<HttpEvent<any>>();
+    }
   }
 
   private getNewTokens(refreshToken: string): Observable<ITokenPair> {
     return this.authService.refresh(refreshToken);
+  }
+
+  private handleRefreshTokenIsNotValid(): Observable<HttpEvent<any>> {
+    this.isRefreshing = false;
+    localStorage.clear();
+    this.store.dispatch(new Logout());
+    this.authService.openAuthModal('sign-in');
+    return of<HttpEvent<any>>();
   }
 }
